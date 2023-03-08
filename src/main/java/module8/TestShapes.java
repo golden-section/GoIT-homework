d52ff8b7bea1733299dd0cd01fdffd5d6d251e00
Alexander <a.gutsol@gmail.com>
2023-03-08 23:29:15 +0200

    Changed TestShapes class to return the shapes name

--- src/main/java/module8/TestShapes.java
+++ src/main/java/module8/TestShapes.java
@@ -3,18 +3,21 @@ package module8;
 public class TestShapes {
     public static void main(String[] args) {
         Shape rect = new Rectangle(10.0, 5.0, "Red");
-        System.out.println(rect);
-
         Shape circle = new Circle(15.0, "Green");
-        System.out.println(circle);
-
         Shape ellipse = new Ellipse(12.0, 7.0, "Blue");
-        System.out.println(ellipse);
-
         Shape triangle = new RightTriangle(6.0, 9.0, "Brown");
-        System.out.println(triangle);
-
         Shape square = new Square(8.0, "Purple");
+
+        System.out.println(rect);
+        System.out.println(circle);
+        System.out.println(ellipse);
+        System.out.println(triangle);
         System.out.println(square);
+
+        System.out.println(rect.name());
+        System.out.println(circle.name());
+        System.out.println(ellipse.name());
+        System.out.println(triangle.name());
+        System.out.println(square.name());
     }
 }
